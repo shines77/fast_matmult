b43ebdde386505af78986850209d0b2e8e151655
shines77 <gz_shines@msn.com>
2014-05-25 21:52:23 +0800

    一处小修改
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/fast_matmult/main.cpp
+++ src/fast_matmult/main.cpp
@@ -293,15 +293,17 @@ int main(int argc, char *argv[])
 
 #if 1
 
-#if defined(LANG_ID) && (LANG_ID != LANG_ZH_CN)
-	printf("Please enter the dimension of the matrix: ");
-    printf("[0 = exit]\n");
-    printf("(must be power of 2, for example: 1024, maximum value is 8192.)\n\n");
-#else
-    printf("请输入矩阵的维数: ");
-    printf("[0 = exit]\n");
-    printf("(必须是2的幂次方, 例如: 1024, 最大值为8192.)\n\n");
-#endif
+    if (lang_id == LANG_ZH_CN) {
+        printf("请输入矩阵的维数: ");
+        printf("[0 = exit]\n");
+        printf("(必须是2的幂次方, 例如: 1024, 最大值为8192.)\n\n");
+    }
+    else {
+	    printf("Please enter the dimension of the matrix: ");
+        printf("[0 = exit]\n");
+        printf("(must be power of 2, for example: 1024, maximum value is 8192.)\n\n");
+    }
+
 	printf("Dim = ? ");
 	scanf_s("%u", &n);
     printf("\n");
