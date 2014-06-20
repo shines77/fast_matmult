879ef253844b731313d3f9ef1356db52f66e8ce5
shines77 <gz_shines@msn.com>
2014-06-21 01:29:26 +0800

    修正stdin.h的_STDINT定义错误.
    Signed-off-by: shines77 <gz_shines@msn.com>

--- include/fast_matmult/vs/stdint.h
+++ include/fast_matmult/vs/stdint.h
@@ -33,8 +33,8 @@
 #error "Use this header only with Microsoft Visual C++ compilers!"
 #endif // _MSC_VER ]
 
-#ifndef _MSC_STDINT_H_ // [
-#define _MSC_STDINT_H_
+#ifndef _STDINT // [
+#define _STDINT
 
 #if _MSC_VER > 1000
 #pragma once
